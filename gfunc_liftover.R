70df55b892db4e3066a6fbd8225d537109e862db
nickb- <nick.dale.burns@gmail.com>
2016-10-03 15:46:53 +1300

    lift over function tested and passed

--- gfunc_liftover.R
+++ gfunc_liftover.R
@@ -1,7 +1,6 @@
 # gfunc_liftover()
 #
-# A function to perform LiftOVer of coordinates
-# from one version of the human genome to another.
+# A function to perform LiftOVer of coordinates.
 # Users Bioconductor's LiftOver function (rtracklayer), which is a
 # native implementation of UCSC's LiftOver command line tool.
 # 
@@ -9,28 +8,43 @@
 # Nick Burns
 # Oct, 2016
 
+# Parameters:
+#   datafile: full path to input datafile
+#   chainfile: full path to appropriate chain file
+#   bed: vector ("CHR", "POS", "SNP") matching the corresponding column names
+#        in the input datafile.
+#
+# Output:
+#   a merged data.table containing only those positions which were mapped over successfully.
 gfunc_liftover <- function (datafile, 
                             chainfile,
-                            bed_columns = c("CHR", "POS", "SNP")) {
+                            bed = c("CHR", "POS", "SNP")) {
     
     data <- data.table::fread(datafile)
-    data.bed <- data[, bed_columns, with = FALSE]
+    data.bed <- data[, bed, with = FALSE]
     
     # reformat the CHR column (prefix: chr)
-    if (any(grepl("chr", data[[bed_columns[1]]]))) {
-        data.bed[, CHR := data[[bed_columns[1]]]]
+    if (any(grepl("chr", data[[bed[1]]]))) {
+        data.bed[, CHR := data[[bed[1]]]]
     } else {
-        data.bed[, CHR := paste0("chr", data[[bed_columns[1]]], sep = "")]
+        data.bed[, CHR := paste0("chr", data[[bed[1]]], sep = "")]
     }
     
-    data_ <- GenomicRanges::GRanges(data.bed[, .(CHR = CHR, 
-                                                 Start = bed_columns[2],
-                                                 End = bed_columns[2] + 1,
-                                                 SNP = bed_columns[3])])
-    data_ <- unlist(rtracklayer::liftOver(data_, 
-                                          rtracklayer::import.chain(chainfile)))
+    # wrangle into GRanges object, then perform liftover
+    data_ <- GenomicRanges::GRanges(data.bed[, .(CHR, 
+                                                 Start = data.bed[[bed[2]]],
+                                                 End = data.bed[[bed[2]]] + 1,
+                                                 data.bed[[bed[3]]])])
+    names(GenomicRanges::mcols(data_)) <- bed[3]
+    data_ <- data.table::as.data.table(
+                rtracklayer::liftOver(data_,
+                                      rtracklayer::import.chain(chainfile))
+                )
     
-    data <- merge(data, data_, by = bed_columns[3])
-    
+    # merge in new coordinates
+    data.table::setkeyv(data_, bed[3])
+    data.table::setkeyv(data, bed[3])
+    data <- data[data_[, .(data_[[bed[3]]], start, end)]]
+  
     return (data)
 }
